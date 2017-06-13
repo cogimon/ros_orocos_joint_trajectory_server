bed87009432c951ae0107e9438e6c7e551e10f1f
Enrico Mingo <enrico.mingo@iit.it>
2017-06-13 11:40:47 +0200

    Added first implementation

---
+++ include/ros_orocos_joint_trajectory_server.h
@@ -0,0 +1,38 @@
+#ifndef ROS_OROCOS_JOINT_TRAJECTORY_SERVER_H_
+#define ROS_OROCOS_JOINT_TRAJECTORY_SERVER_H_
+
+#include <rtt/RTT.hpp>
+#include <string>
+#include <rst-rt/robot/JointState.hpp>
+#include <rst-rt/kinematics/JointAngles.hpp>
+#include <rtt/TaskContext.hpp>
+#include <rtt/Component.hpp>
+#include <rtt/Port.hpp>
+#include <urdf/model.h>
+#include <trajectory_msgs/JointTrajectory.h>
+
+class ros_orocos_joint_trajectory_server: public RTT::TaskContext{
+public:
+    ros_orocos_joint_trajectory_server(std::string const & name);
+
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+
+private:
+    bool attachToRobot(const std::string& robot_name);
+
+    std::string _robot_name;
+    std::map<std::string, std::vector<std::string> > _map_kin_chains_joints;
+    std::map<std::string, boost::shared_ptr<RTT::OutputPort<rstrt::kinematics::JointAngles> > > _kinematic_chains_output_ports;
+    std::map<std::string, rstrt::kinematics::JointAngles> _kinematic_chains_desired_joint_state_map;
+
+    RTT::InputPort<trajectory_msgs::JointTrajectory::Ptr> _joint_trajectory_port;
+    trajectory_msgs::JointTrajectory::Ptr _joint_trajectory_msg;
+
+    bool _send_joint_trj;
+    bool _trj_counter;
+};
+
+
+#endif
---
+++ src/ros_orocos_joint_trajectory_server.cpp
@@ -0,0 +1,109 @@
+#include <ros_orocos_joint_trajectory_server.h>
+#include <rtt/plugin/PluginLoader.hpp>
+#include <ros/ros.h>
+#include <rtt_rosclock/rtt_rosclock.h>
+#include <rtt_roscomm/rtt_rostopic.h>
+#include <algorithm>
+
+ros_orocos_joint_trajectory_server::ros_orocos_joint_trajectory_server(std::string const & name):
+    RTT::TaskContext(name),
+    _send_joint_trj(false),
+    _trj_counter(0)
+{
+    this->setActivity(new RTT::Activity(1, 0.01));
+
+    this->addOperation("attachToRobot", &ros_orocos_joint_trajectory_server::attachToRobot,
+                this, RTT::ClientThread);
+}
+
+bool ros_orocos_joint_trajectory_server::attachToRobot(const std::string &robot_name)
+{
+    _robot_name = robot_name;
+    RTT::log(RTT::Info)<<"Robot name: "<<_robot_name<<RTT::endlog();
+
+    RTT::TaskContext* task_ptr = this->getPeer(robot_name);
+    if(!task_ptr){
+        RTT::log(RTT::Error)<<"Can not getPeer("<<robot_name<<")"<<RTT::endlog();
+        return false;}
+
+    RTT::log(RTT::Info)<<"Found Peer "<<robot_name<<RTT::endlog();
+
+    RTT::OperationCaller<std::map<std::string, std::vector<std::string> >(void) > getKinematicChainsAndJoints
+        = task_ptr->getOperation("getKinematicChainsAndJoints");
+
+    _map_kin_chains_joints = getKinematicChainsAndJoints();
+
+    std::map<std::string, std::vector<std::string> >::iterator it;
+    for(it = _map_kin_chains_joints.begin(); it != _map_kin_chains_joints.end(); it++)
+    {
+        std::string kin_chain_name = it->first;
+        std::vector<std::string> joint_names = it->second;
+
+
+        _kinematic_chains_output_ports[kin_chain_name] =
+                boost::shared_ptr<RTT::OutputPort<rstrt::kinematics::JointAngles> >(
+                            new RTT::OutputPort<rstrt::kinematics::JointAngles>(
+                                kin_chain_name+"_"+"JointPositionCtrl"));
+        this->addPort(*(_kinematic_chains_output_ports.at(kin_chain_name))).
+                doc(kin_chain_name+"_"+"JointPositionCtrl port");
+        _kinematic_chains_output_ports.at(kin_chain_name)->connectTo(
+                    task_ptr->ports()->getPort(kin_chain_name+"_"+"JointPositionCtrl"));
+
+
+        rstrt::kinematics::JointAngles tmp(joint_names.size());
+        _kinematic_chains_desired_joint_state_map[kin_chain_name] = tmp;
+
+
+        RTT::log(RTT::Info)<<"Added "<<kin_chain_name<<" port and data"<<RTT::endlog();
+    }
+
+    return true;
+}
+
+bool ros_orocos_joint_trajectory_server::configureHook()
+{
+    this->addPort(_joint_trajectory_port).doc("Joint Trajectory from ROS node");
+
+    return true;
+}
+
+bool ros_orocos_joint_trajectory_server::startHook()
+{
+    _joint_trajectory_port.createStream(rtt_roscomm::topic("joint_trajectory"));
+
+    return true;
+}
+
+void ros_orocos_joint_trajectory_server::updateHook()
+{
+    RTT::FlowStatus fs = _joint_trajectory_port.read(_joint_trajectory_msg);
+    if(fs == 2)
+    {
+        _send_joint_trj = true;
+        _trj_counter = 0;
+    }
+
+    if(_send_joint_trj && _joint_trajectory_msg)
+    {
+        std::map<std::string, boost::shared_ptr<RTT::OutputPort<rstrt::kinematics::JointAngles> > >::iterator it;
+        for(it = _kinematic_chains_output_ports.begin(); it != _kinematic_chains_output_ports.end(); it++)
+        {
+            for(unsigned int i = 0; i < _map_kin_chains_joints.at(it->first).size(); ++i)
+            {
+                int id = std::distance(_joint_trajectory_msg->joint_names.begin(),
+                                       std::find(_joint_trajectory_msg->joint_names.begin(), _joint_trajectory_msg->joint_names.end(),
+                                       _map_kin_chains_joints.at(it->first)[i]));
+                _kinematic_chains_desired_joint_state_map.at(it->first).angles[i] =
+                        _joint_trajectory_msg->points[_trj_counter].positions[id];
+            }
+
+            _kinematic_chains_output_ports.at(it->first)->write(
+                        _kinematic_chains_desired_joint_state_map.at(it->first));
+        }
+        _trj_counter++;
+
+        if(_trj_counter >= _joint_trajectory_msg->points.size())
+            _send_joint_trj = false;
+    }
+
+}
